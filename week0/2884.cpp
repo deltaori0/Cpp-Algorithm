470e8ef5820bc539928db70731c5033a86f474c8
Eunsoo <deltaori0@korea.ac.kr>
2021-04-01 02:19:27 +0900

    알람 시계

---
+++ week0/2884.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int h, m;
+	scanf("%d %d", &h, &m);
+
+	if (m < 45) {
+		m += 15;
+		h--;
+		if (h < 0)	h = 23;
+	}
+	else {
+		m -= 45;
+	}
+
+	printf("%d %d", h, m);
+}
