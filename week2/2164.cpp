4ec2e1f6741e9d39a862d677a59b008dfdd72222
Eunsoo <deltaori0@korea.ac.kr>
2021-05-06 18:08:01 +0900

    카드2

---
+++ week2/2164.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	freopen("input.txt", "r", stdin);
+
+	queue <int> q;
+	int n, temp;
+	cin >> n;
+	for (int i = 1; i <= n; i ++)
+		q.push(i);
+	
+	while(q.size() > 1){
+		q.pop();
+		q.push(q.front());
+		q.pop();
+	}
+
+	cout << q.front() << "\n";
+
+}
